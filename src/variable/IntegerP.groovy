d66b666abbb07676e861244ef2d48e84be60e14c
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-23 00:51:17 +0900

    내용 추가

--- src/variable/IntegerP.groovy
+++ src/variable/IntegerP.groovy
@@ -1,4 +1,29 @@
 package variable
 
-class IntegerP {
+import item.Check
+import item.KeyValueItem
+import item.Setting
+import item.work.VariableWork
+
+import java.util.regex.Pattern
+
+class IntegerP extends Setting implements Check, VariableWork {
+    public static final String SPECIFIED = "ㅇㅈㅇ"
+    public static final String patternText = "(\\n|^)\\s*ㅇㅈㅇ\\s"
+    private final Pattern pattern = Pattern.compile patternText
+
+    @Override
+    boolean check(String line) {
+        return pattern.matcher(line).find()
+    }
+
+    @Override
+    void start(String line) throws Exception {
+        KeyValueItem keyValue = setKeyValue SPECIFIED, line
+        String key = keyValue.getKey()
+        String value = keyValue.getValue()
+        if (!varCheck.check(line, VarType.Integer)) throw new Exception(typeErrorMessage)
+        IM.put key, Integer.valueOf(value)
+        set.add key
+    }
 }
