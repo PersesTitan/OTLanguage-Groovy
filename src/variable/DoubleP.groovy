f41947f74e1af83990de1c9ab344b449e5c163c0
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-23 00:51:12 +0900

    내용 추가

--- src/variable/DoubleP.groovy
+++ src/variable/DoubleP.groovy
@@ -1,4 +1,29 @@
 package variable
 
-class DoubleP {
+import item.Check
+import item.KeyValueItem
+import item.Setting
+import item.work.VariableWork
+
+import java.util.regex.Pattern
+
+class DoubleP  extends Setting implements Check, VariableWork {
+    public static final String SPECIFIED = "ㅇㅆㅇ"
+    public static final String patternText = "(\\n|^)\\s*ㅇㅆㅇ\\s"
+    private final Pattern pattern = Pattern.compile patternText
+
+    @Override
+    boolean check(String line) {
+        return pattern.matcher(line).find()
+    }
+
+    @Override
+    void start(String line) throws Exception {
+        KeyValueItem keyValue = setKeyValue SPECIFIED, line
+        String key = keyValue.getKey()
+        String value = keyValue.getValue()
+        if (!varCheck.check(value, VarType.Double)) throw new Exception(typeErrorMessage)
+        DM.put key, Double.valueOf(value)
+        set.add key
+    }
 }
