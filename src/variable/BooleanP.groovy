5405fb0b862b21ecce93a2d306fa470d42203d1e
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-23 00:51:05 +0900

    내용 추가

--- src/variable/BooleanP.groovy
+++ src/variable/BooleanP.groovy
@@ -10,7 +10,7 @@ import java.util.regex.Pattern
 class BooleanP extends Setting implements Check, VariableWork {
     private static final SPECIFIED = "ㅇㅂㅇ"
     private static final String patternText = "(\\n|^)\\s*ㅇㅂㅇ\\s"
-    private final Pattern pattern = Pattern.compile(patternText)
+    private final Pattern pattern = Pattern.compile patternText
 
     @Override
     boolean check(String line) throws Exception {
@@ -19,13 +19,13 @@ class BooleanP extends Setting implements Check, VariableWork {
 
     @Override
     void start(String line) throws Exception {
-        KeyValueItem keyValue = setKeyValue(SPECIFIED, line);
-        String key = keyValue.getKey();
-        String value = keyValue.getValue();
-        value = value.replace("ㅇㅇ", "true");
-        value = value.replace("ㄴㄴ", "false");
-        value = value.replace(" ", "");
-        BM.put(key, changeBool(value));
-        set.add(key);
+        KeyValueItem keyValue = setKeyValue SPECIFIED, line
+        String key = keyValue.getKey()
+        String value = keyValue.getValue()
+        value = value.replace "ㅇㅇ", "true"
+        value = value.replace "ㄴㄴ", "false"
+        value = value.replace " ", ""
+        BM.put key, changeBool(value)
+        set.add key
     }
 }
