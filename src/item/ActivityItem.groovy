fdf5ce0e0e9fd011177ab135f0556f6b0b47f1c9
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-23 00:50:50 +0900

    각 변수 추가

--- src/item/ActivityItem.groovy
+++ src/item/ActivityItem.groovy
@@ -3,9 +3,24 @@ package item
 import print.Print
 import print.Println
 import print.ScannerP
+import variable.BooleanP
+import variable.CharacterP
+import variable.DoubleP
+import variable.FloatP
+import variable.IntegerP
+import variable.LongP
+import variable.StringP
 
 interface ActivityItem {
     ScannerP scannerP = new ScannerP()
     Print print = new Print()
     Println println = new Println()
+    /*=================================*/
+    BooleanP booleanP = new BooleanP()
+    CharacterP characterP = new CharacterP()
+    DoubleP doubleP = new DoubleP()
+    FloatP floatP = new FloatP()
+    IntegerP integerP = new IntegerP()
+    LongP longP = new LongP()
+    StringP stringP = new StringP()
 }
