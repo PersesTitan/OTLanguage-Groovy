a9e8a70733ae28e8e9e0f2b43020acb1fd4aa423
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-23 00:51:22 +0900

    내용 추가

--- src/variable/LongP.groovy
+++ src/variable/LongP.groovy
@@ -1,4 +1,29 @@
 package variable
 
-class LongP {
+import item.Check
+import item.KeyValueItem
+import item.Setting
+import item.work.VariableWork
+
+import java.util.regex.Pattern
+
+class LongP extends Setting implements Check, VariableWork {
+    public static final String SPECIFIED = "ㅇㅉㅇ"
+    public static final String patternText = "(\\n|^)\\s*ㅇㅉㅇ\\s"
+    private final Pattern pattern = Pattern.compile patternText
+
+    @Override
+    boolean check(String line) {
+        return pattern.matcher(line).find()
+    }
+
+    @Override
+    void start(String line) throws Exception {
+        KeyValueItem keyValue = setKeyValue SPECIFIED, line
+        String key = keyValue.getKey()
+        String value = keyValue.getValue()
+        if (!varCheck.check(line, VarType.Long)) throw new Exception(typeErrorMessage)
+        LM.put key, Long.valueOf(value)
+        set.add key
+    }
 }
