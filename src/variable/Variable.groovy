b6be93e572a2e355677f62389bd17084e894d9f5
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-26 17:09:26 +0900

    Variable check로직 및 getVar 추가

--- src/variable/Variable.groovy
+++ src/variable/Variable.groovy
@@ -1,5 +1,35 @@
 package variable
 
-class Variable {
+import item.Check
+import item.Setting
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class Variable extends Setting implements Check {
+    private final String text = ":\\S+ ";
+    private final Pattern pattern = Pattern.compile(text);
+
+    String getVar(String line) {
+        Matcher m = pattern.matcher(line);
+        while(m.find()) {
+            String origin = m.group();
+            String key = origin.strip().substring(1);
+            if (set.contains(key))
+                line = line.replaceFirst(origin, checkValue(key));
+        }
+        return line;
+    }
+
+    @Override
+    boolean check(String line) {
+        if (line == null || line.isEmpty()) return false;
+        boolean bool = pattern.matcher(line).find();
+
+        var lines = line.split(" ");
+        return bool || Arrays.stream(lines)
+                .filter(v -> !v.isEmpty())
+                .filter(v -> v.startsWith(":"))
+                .anyMatch(v -> set.contains(v.substring(1)));
+    }
 }
